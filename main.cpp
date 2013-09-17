7b5abf3b5201957272e0bfeb6ea9c732dc9e4617
vasyasv <vasyasv@mail.ru>
2013-09-17 18:41:46 +0400

    V.2.0 Relise Calculeted befor 100000 found roundlye long 5

--- main.cpp
+++ main.cpp
@@ -3,15 +3,23 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <conio.h>
 using namespace std;
-typedef unsigned long int uli;
-const uli LAST_ELEMENT = 10000000;// 0.5 for max
+typedef /*unsigned long*/ int uli;
+const uli LAST_ELEMENT = 100000;// 0.5 for max
+// el roundleys
+struct el_round{
+    uli ch;
+    uli last_ch;
+};
 
-void kill(list <uli> list_sacrifises, list <uli> &real_sacrifises){
+void kill(list <uli> list_sacrifises, list <el_round> &real_sacrifises){
     // убиваем всех по списку
+    if (list_sacrifises.empty())
+        return;
     for (list <uli>::iterator i = list_sacrifises.begin(); i != list_sacrifises.end(); ++i)
-        for (list <uli>::iterator j = real_sacrifises.begin(); j != real_sacrifises.end(); ++j)
-            if (*i==*j)
+        for (list <el_round>::iterator j = real_sacrifises.begin(); j != real_sacrifises.end(); ++j)
+            if ((*i)==(*j).ch)
             {
                 real_sacrifises.erase(j);
                 break;
@@ -50,38 +58,62 @@ uli sum_div(uli num){ // Сложность O(sqrt(n))
 
 uli sum_befor_div(uli num){
     uli temp = sum_div(num)-num;
-    if (num > LAST_ELEMENT)
+    if (num > LAST_ELEMENT || temp < 1)
         return 1;
     return temp;
 }
 
-bool found(uli element, list <uli> l){
-    for (list <uli>::iterator i = l.begin(); i != l.end(); ++i)
-        if (*i == element)
+bool found(uli element, list <el_round> l){
+    for (list <el_round>::iterator i = l.begin(); i != l.end(); ++i)
+        if ((*i).ch == element)
             return true;
     return false;
 }
 
-void merge(list <uli> roundlaye, list <uli> &l_roundlayes){
+void clear_l_roundlayes(list <el_round> &l_roundlayes, uli cur){
+    list <uli> kill_list;
+    for (list <el_round>::iterator i = l_roundlayes.begin(); i != l_roundlayes.end() ; ++i)
+        if ((*i).last_ch > cur)
+            continue;
+        else{ // иначе перещитываем
+            uli bouble = sum_befor_div((*i).last_ch);
+            for (; bouble < cur && found(bouble, l_roundlayes); bouble = sum_befor_div(bouble))
+                ;
+            (*i).last_ch = bouble;
+            if (bouble < cur)
+                kill_list.push_back((*i).ch);
+            //cout << (*i).ch;
+        }
+    kill(kill_list, l_roundlayes);
+}
+
+void merge(list <uli> roundlaye, list <el_round> &l_roundlayes, uli cur){
     if (roundlaye.empty())
         return;
+    clear_l_roundlayes(l_roundlayes, cur);
     for (list <uli>::iterator i = roundlaye.begin(); i != roundlaye.end(); ++i)
     {
-        list <uli>::iterator j = l_roundlayes.begin();
+        list <el_round>::iterator j = l_roundlayes.begin();
         for (; j != l_roundlayes.end(); ++j)
-            if (*i==*j)
+            if (*i == (*j).ch)
                 break;
         if (j == l_roundlayes.end()) // нет таких как i
-            l_roundlayes.push_back(*i);
+        {
+            el_round tmp;
+            tmp.ch = *i;
+            tmp.last_ch = roundlaye.back();
+            l_roundlayes.push_back(tmp);
+        }
     }
 }
 
+
 int main()
 {
-    list <uli> l_roundlayes; // части хороводов в том числе и просто перспективные
+    list <el_round> l_roundlayes; // части хороводов в том числе и просто перспективные
     list <uli> max_roundlaye; // максимальный хоровод
-    cout << sum_befor_div(1210) << " ";
-    cout << sum_befor_div(1184);
+    cout << sum_befor_div(4600);
+    cout << " " << sum_befor_div(sum_befor_div(4600)) ;
 
 
     for (uli cur = 1; l_roundlayes.size() < LAST_ELEMENT; cur++)
@@ -92,7 +124,10 @@ int main()
         {
             if (buble > cur) // цепочка возможно нас устраивает, может она хоровод? пусть живет, пака
             {
-                l_roundlayes.push_back(buble);
+                el_round tmp;
+                tmp.ch = cur;
+                tmp.last_ch = buble;
+                l_roundlayes.push_back(tmp);
                 break;
             }
             if (buble == 1 || buble == cur || !found(buble, l_roundlayes)) // цепь ведет ясно куда..
@@ -108,13 +143,16 @@ int main()
             }
             cur_roundlaye.push_back(buble);
         }
-        merge(cur_roundlaye, l_roundlayes);// добавим элементы в l_roundlayes
+        merge(cur_roundlaye, l_roundlayes, cur);// добавим элементы в l_roundlayes
+        if (cur % 1000 == 0)
+            cout << cur << " max = " << max_roundlaye.size() << "\n";
     }
     cout << "found roundlaye for diapozon [1.." << LAST_ELEMENT <<"]\n" ;
     cout << "it's long = " << max_roundlaye.size() << "\n";
     cout << "roundlaye: ";
     for (list <uli>::iterator i = max_roundlaye.begin(); i != max_roundlaye.end(); ++i)
         cout << *i << " ";
+    _getch();
     return 0;
 }
 
